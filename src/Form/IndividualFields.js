b5ab62f02f4ef2c2c7af411d70f0bdf5d0860996
Jonathan Cockrem <jonathan.cockrem@focisolutions.com>
2018-04-14 17:54:38 -0400

    Auto stash before merge of "master" and "origin/master"

---
+++ src/Form/IndividualFields.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Row, Input} from 'react-materialize';
+
+
+class IndividualFields  extends Component {
+
+  render() {
+    return (
+      <Row>
+            <Input s={6} label="Contact First Name" />
+            <Input s={6} label="Last Name" />
+            <Input s={6} label="Street Address"/>
+            <Input s={6} label="City"/>
+            <Input s={6} label="Province"/>
+            <Input s={6} label="Postal Code" s={6}/>          
+            <Input s={6} type="email" label="Email" s={12} />
+            <Input s={6} label="phone" s={12} />
+            <Input s={6} label="Website"/>
+
+        </Row>
+    );
+  }
+}
+
+export default IndividualFields;
