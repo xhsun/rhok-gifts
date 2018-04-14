000ae2dbf732f8054c787534808a531a62cf4107
Hannah Sun <hannah.sun@focisolutions.com>
2018-04-14 16:04:18 -0400

    update forms

--- src/App/App.js
+++ src/App/App.js
@@ -2,17 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Form from '../Form/Form.js'
-import {Button} from 'react-materialize';
+import {Button, Modal} from 'react-materialize';
 
 class App extends Component {
+  constructor(){
+        super();
+        this.state = {showForm:false}
+    }
 
+    handleClick=()=>{
 
-
+      this.setState({showForm: true});
+    }
 
   render() {
     return (
       <div className="App">
+        <Modal header='Donate' trigger={<Button>Donate</Button>}>
         
+        </Modal>
       </div>
     );
   }
