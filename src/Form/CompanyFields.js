18805952ce72893223b42f809f2b39beb9ea367e
Jonathan Cockrem <jonathan.cockrem@focisolutions.com>
2018-04-14 16:04:53 -0400

    Update Company Fields testing

--- src/Form/CompanyFields.js
+++ src/Form/CompanyFields.js
@@ -9,11 +9,16 @@ class CompanyFields  extends Component {
       <Row>
             <Input placeholder="Placeholder" s={6} label="First Name" />
             <Input s={6} label="Last Name" />
+            <Input label="Company Name"/>
             <Input type="email" label="Email" s={12} />
             <Input label="phone" s={12} />
             <Input label="neighbour" s={12} />
             <Input label="Add Item" s={12} />
-          </Row>
+            <label> Donate Before ?</label>
+            <Input type="radio" value="option1" checked={this.state.selectedOption === 'option1'}/>
+            <Input type="radio" value="option2" checked={this.state.selectedOption === 'option2'}/>
+            <Input type="radio" value="option3" checked={this.state.selectedOption === 'option3'}/>
+        </Row>
     );
   }
 }
